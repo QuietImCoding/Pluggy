8e8169be66c5f70f4abac4116620b108192e4438
Daniel Montesomething <dmonte3210@gmail.com>
2018-07-20 10:49:20 -0400

    Compiled latest version of pluggy

--- compile_pluggy.bat
+++ compile_pluggy.bat
@@ -1,6 +1,18 @@
 @echo off
+
+:build
+del pluggy.zip
 pyinstaller -y -i pluggy.ico -w pluggy.py
 copy pluggy.ico dist\pluggy
 copy chromedriver.exe dist\pluggy
 copy pluggy.png dist\pluggy
-pause
+move dist\pluggy .
+
+:cleanup
+del /s /f /q build
+del /s /f /q dist
+del /s /f /q __pycache__
+rmdir /s /q build
+rmdir /s /q dist
+rmdir /s /q __pycache__
+del pluggy.spec
